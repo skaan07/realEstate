6f0a762851c16d902ba1afb51d06c5c10614795b
skaan07 <sekaan07@gmail.com>
2017-03-16 16:34:47 +0300

    --CSV FILE ADDED

---
+++ Realestate/src/main/java/edu/metu/ceng/realestate/util/DataImportUtil.java
@@ -0,0 +1,12 @@
+package edu.metu.ceng.realestate.util;
+
+import java.util.List;
+
+public class DataImportUtil {
+
+	public static List<Object> importDataFromCsv (String filePath){
+		
+		return null;
+		
+	}
+}
